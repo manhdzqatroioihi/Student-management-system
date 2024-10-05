3d77c505cf04e4bf0451423c97f6832a150a4fbd
Hoàng Quốc Mạnh <119374719+manhdzqatroioihi@users.noreply.github.com>
2024-10-05 10:06:59 +0700

    update file database py

--- database_str.py
+++ database_str.py
@@ -2,6 +2,6 @@ class Database_str:
     def __init__(self):
         self.host='localhost'
         self.user='root'
-        self.password='123456a@'
+        self.password='mat khau sql cua ban'
         self.database='manh'
         self.port='3306'
